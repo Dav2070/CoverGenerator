d00a148e7272a4c16fa2fba8c7ff3b327ecca50b
David <dav2070@web.de>
2021-05-12 22:34:37 +0200

    Install and configure bootstrap, ngx-file-helpers and blurhash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgxFileHelpersModule } from 'ngx-file-helpers'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		NgbModule,
+		NgxFileHelpersModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
