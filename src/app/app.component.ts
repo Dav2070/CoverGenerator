cb224c8c30f081c0b9655605397b76175a54f705
David <dav2070@web.de>
2021-05-12 22:35:03 +0200

    Add basic layout for cover generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ReadFile } from 'ngx-file-helpers'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cover-generator';
+
+	ImageFilePicked(file: ReadFile) {
+		
+	}
 }
