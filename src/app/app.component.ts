f39c0250f9a4f8161317650152c412e002171fb9
David <dav2070@web.de>
2021-05-13 23:35:48 +0200

    Implement functionality for showing the selected image on the canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, ViewChild } from '@angular/core'
 import { ReadFile } from 'ngx-file-helpers'
+import { BlobToBase64, PromiseHolder } from 'dav-js'
 
 @Component({
 	selector: 'app-root',
@@ -7,8 +8,37 @@ import { ReadFile } from 'ngx-file-helpers'
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	@ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>
+	canvasContext: CanvasRenderingContext2D
+	imageWidth: number = 500
+	imageHeight: number = 500
 
-	ImageFilePicked(file: ReadFile) {
-		
+	ngOnInit() {
+		this.canvasContext = this.canvas.nativeElement.getContext('2d')
+	}
+
+	async ImageFilePicked(file: ReadFile) {
+		this.canvasContext.clearRect(0, 0, this.imageWidth, this.imageHeight)
+
+		let imageBlob = new Blob([file.underlyingFile], { type: file.type })
+		let imageBase64 = await BlobToBase64(imageBlob)
+
+		let image = new Image()
+		let imageLoadPromiseHolder = new PromiseHolder()
+
+		image.onload = () => imageLoadPromiseHolder.resolve()
+		image.src = imageBase64
+		await imageLoadPromiseHolder.AwaitResult()
+
+		this.imageWidth = image.width
+		this.imageHeight = image.height
+
+		this.canvas.nativeElement.setAttribute("width", this.imageWidth.toString())
+		this.canvas.nativeElement.setAttribute("height", this.imageHeight.toString())
+
+		this.canvasContext.drawImage(image,
+			0, 0, this.imageWidth - 1, this.imageHeight - 1,
+			0, 0, this.imageWidth, this.imageHeight
+		)
 	}
 }
