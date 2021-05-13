eca1382a5dd2e4f7cd722f0352bb4b1924a6bb45
David <dav2070@web.de>
2021-05-14 00:51:56 +0200

    Implement generating the blurhash and adding the blurhashed bottom part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core'
 import { ReadFile } from 'ngx-file-helpers'
+import { decode, encode } from 'blurhash'
 import { BlobToBase64, PromiseHolder } from 'dav-js'
 
 @Component({
@@ -20,6 +21,7 @@ export class AppComponent {
 	async ImageFilePicked(file: ReadFile) {
 		this.canvasContext.clearRect(0, 0, this.imageWidth, this.imageHeight)
 
+		// Read the selected imgae file
 		let imageBlob = new Blob([file.underlyingFile], { type: file.type })
 		let imageBase64 = await BlobToBase64(imageBlob)
 
@@ -33,6 +35,7 @@ export class AppComponent {
 		this.imageWidth = image.width
 		this.imageHeight = image.height
 
+		// Set the correct dimensions of the canvas and draw the image
 		this.canvas.nativeElement.setAttribute("width", this.imageWidth.toString())
 		this.canvas.nativeElement.setAttribute("height", this.imageHeight.toString())
 
@@ -40,5 +43,19 @@ export class AppComponent {
 			0, 0, this.imageWidth - 1, this.imageHeight - 1,
 			0, 0, this.imageWidth, this.imageHeight
 		)
+
+		// Generate the blurhash for the bottom part
+		let bottomPartHeight = this.imageHeight * 0.24
+		let bottomPartStart = this.imageHeight - bottomPartHeight
+		let imageData = this.canvasContext.getImageData(0, bottomPartStart, this.imageWidth, bottomPartHeight)
+
+		let blurhash = encode(imageData.data, imageData.width, imageData.height, 4, 3)
+
+		let blurhashPixels = decode(blurhash, this.imageWidth, bottomPartHeight)
+		let blurhashImageData = this.canvasContext.createImageData(this.imageWidth, bottomPartHeight)
+		blurhashImageData.data.set(blurhashPixels)
+
+		// Add the blurhashed bottom part to the canvas
+		this.canvasContext.putImageData(blurhashImageData, 0, bottomPartStart)
 	}
 }
