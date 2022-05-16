a68e7d8249b152dd38740afbffab4789cdece123
David <dav2070@web.de>
2022-05-16 23:27:41 +0200

    Setup routing and add empty cover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { NgxFileHelpersModule } from 'ngx-file-helpers'
 
 import { AppComponent } from './app.component'
+import { CoverComponent } from './cover/cover.component'
+
+const routes: Routes = [
+	{ path: "", component: CoverComponent }
+]
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		CoverComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
+		RouterModule.forRoot(routes),
 		NgbModule,
 		NgxFileHelpersModule
 	],
