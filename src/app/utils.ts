30273a483e71268f3b14a16172cbf4db6560cf87
David Zurek <dav2070@web.de>
2022-11-05 21:56:48 +0100

    Add function for retrieving the cover dimensions from BoD
    - Install axios for the HTTP request and upgrade dav-js

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { defaultAuthorNameFontSize } from './constants'
 
 export function CalculateFontSizeRelativeToHeight(originalFontSize: number, resultHeight: number): number {
@@ -6,4 +7,57 @@ export function CalculateFontSizeRelativeToHeight(originalFontSize: number, resu
 
 export function GetDefaultAuthorNameFontSize(height: number): number {
 	return CalculateFontSizeRelativeToHeight(defaultAuthorNameFontSize, height)
+}
+
+export async function GetCoverDimensions(): Promise<{
+	rueckenbreite: number,
+	beschnitt: number,
+	ulbreite_o: number,
+	ulbreite_m: number,
+	gbreite_o: number,
+	gbreite_m: number,
+	hoehe_o: number,
+	hoehe_m: number,
+	dicke: number,
+	su_max_gbreite_m: number
+}> {
+	try {
+		let response = await axios({
+			method: 'post',
+			url: 'https://www.bod.de/fileadmin/bod/php/include/utils.ajax.php',
+			params: new URLSearchParams({
+				MODE: 'bp_checkcover',
+				einbandart: 'PB',
+				buchformat: '12*19',
+				anzseiten: '342',
+				papierfarbe: 'weiss80'
+			})
+		})
+
+		return {
+			rueckenbreite: Number.parseFloat(response.data.rueckenbreite),
+			beschnitt: Number.parseFloat(response.data.beschnitt),
+			ulbreite_o: Number.parseFloat(response.data.ulbreite_o),
+			ulbreite_m: Number.parseFloat(response.data.ulbreite_m),
+			gbreite_o: Number.parseFloat(response.data.gbreite_o),
+			gbreite_m: Number.parseFloat(response.data.gbreite_m),
+			hoehe_o: Number.parseFloat(response.data.hoehe_o),
+			hoehe_m: Number.parseFloat(response.data.hoehe_m),
+			dicke: Number.parseFloat(response.data.dicke),
+			su_max_gbreite_m: response.data.su_max_gbreite_m
+		}
+	} catch (error) {
+		return {
+			rueckenbreite: 0,
+			beschnitt: 0,
+			ulbreite_o: 0,
+			ulbreite_m: 0,
+			gbreite_o: 0,
+			gbreite_m: 0,
+			hoehe_o: 0,
+			hoehe_m: 0,
+			dicke: 0,
+			su_max_gbreite_m: 0
+		}
+	}
 }
