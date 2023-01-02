4ebd128eebe643437e10dd5186d975adfadc3c80
David <zurek.dav@gmail.com>
2023-01-02 23:16:40 +0100

    Add empty EpubToDocx page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,20 @@ import { NgxFileHelpersModule } from 'ngx-file-helpers'
 import { AppComponent } from './app.component'
 import { CoverComponent } from './cover/cover.component';
 import { PrintCoverComponent } from './print-cover/print-cover.component'
+import { EpubToDocxComponent } from './epub-to-docx/epub-to-docx.component'
 
 const routes: Routes = [
 	{ path: "", component: CoverComponent },
-	{ path: "print", component: PrintCoverComponent }
+	{ path: "print", component: PrintCoverComponent },
+	{ path: "epub-to-docx", component: EpubToDocxComponent }
 ]
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		CoverComponent,
-		PrintCoverComponent
+		PrintCoverComponent,
+		EpubToDocxComponent
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/epub-to-docx/epub-to-docx.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+   selector: 'app-epub-to-docx',
+   templateUrl: './epub-to-docx.component.html'
+})
+export class EpubToDocxComponent {
+   
+}
